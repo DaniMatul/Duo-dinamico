d660b0229f7994951638dad8f0e685ae741ab429
DaniMatul <mayda.matul@gmail.com>
2023-03-14 10:09:02 -0600

    serie VIII
    MODIFICACION DEL CODIGO

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ residuo = 0
 #Division del mayor 2
 residuo = mayor % 2
 if residuo == 0:
-    print("El número mayor es par")
+    print("El número mayor es par: ")
 else:
-    print("El número mayor es impar")
+    print("El número mayor es impar: ")
